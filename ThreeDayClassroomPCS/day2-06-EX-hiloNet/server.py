98bed207f85ce38475e2ec5f7e15cede08e154db
Chris <topherCantrell@gmail.com>
2019-07-30 15:14:53 -0500

    Minimal solution

--- ThreeDayClassroomPCS/day2-06-EX-hiloNet/server.py
+++ ThreeDayClassroomPCS/day2-06-EX-hiloNet/server.py
@@ -1,4 +1,6 @@
 import socket
+import random
+import net_utils
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -6,12 +8,27 @@ sock.bind( ('',7777) )
 
 sock.listen(1)
 
-client,addr = sock.accept()
+while True:
+    
+    print('Waiting on connection')
+    client,addr = sock.accept()
+    print('Serving',addr)
+    
+    my_number = random.randint(1,101)
+    
+    while True:
+        
+        guess = net_utils.recv_string(client)
+        guess = int(guess)
+        
+        if guess < my_number:
+            net_utils.send_string(client,'Higher!')
+        if guess > my_number:
+            net_utils.send_string(client,'Lower!')
+        if guess == my_number:
+            net_utils.send_string(client,'You got it!')
+            client.close()
+            break
+            
 
-print('Got a connection!',addr)
-
-import net_utils
-
-s = net_utils.recv_string(client)
-print('::'+s+'::')
 
